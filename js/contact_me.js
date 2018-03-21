238d0470053ec51a33a963e465590c08517796f7
SherMH <mamanihuancasherli@gmail.com>
2018-03-21 18:24:04 -0400

    cambio 4 direccion mensaje
    inscripcion en castellano

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Tu mensaje fue enviado correctamente. </strong>");
+                        .append("<strong>Tu mensaje fue enviado correctamente. no olviden inscribirse en la parte superior </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +45,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Lo siento" + firstName + ", tu correo no pudo ser enviado!");
+                    $('#success > .alert-danger').append("<strong>Lo siento " + firstName + ", tu correo no pudo ser enviado!, no olviden inscribirse en la parte superior");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
